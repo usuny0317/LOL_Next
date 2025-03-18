a0419d8c1781f036f159cacc66657188dde501ce
usuny0317 <usuny0317@naver.com>
2025-03-18 17:01:41 +0900

    fix/ env 파일에 NEXT_PUBLIC_없애서 보안 높히기!

--- src/app/api/handleroute/route.ts
+++ src/app/api/handleroute/route.ts
@@ -3,17 +3,14 @@ export async function GET() {
     "https://br1.api.riotgames.com/lol/platform/v3/champion-rotations",
     {
       headers: {
-        "X-Riot-Token": `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
+        "X-Riot-Token": `${process.env.RIOT_API_KEY}`,
         "Content-Type": "application/json",
         "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
       },
     }
   );
   if (!res) {
-    return new Response(JSON.stringify({ error: "데이터 불러오기 실패" }), {
-      status: 400,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(JSON.stringify({ error: "데이터 불러오기 실패" }));
   }
   const data = await res.json();
   return Response.json({ data });
